9e4d8d1294a0fe061829afbb9b3b31c47b7ccd0f
kri-ruj <krittameth.rujirachainon@gmail.com>
2025-06-11 17:54:47 +0700

    v3.7.5: Add ultra-compact quick add UI with floating action button

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const CONFIG = {
     liffId: '2007552096-wrG1aV9p', // Production LIFF ID
     apiBaseUrl: 'https://salesappfkt.as.r.appspot.com/api', // GCP Production API URL
     teamId: null, // Will be set automatically from group chat
-    VERSION: '3.6.8' // Current version
+    VERSION: '3.7.5' // Current version - Added ultra-compact quick add UI
 };
 
 // Export for use in other files
