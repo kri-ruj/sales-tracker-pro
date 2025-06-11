cdbd20b9e8757088fd42b8fdf2d0a74f28ee01c4
kri-ruj <krittameth.rujirachainon@gmail.com>
2025-06-11 17:16:45 +0700

    Fix backend URL to use main domain instead of service-specific URL

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const CONFIG = {
     liffId: '2007552096-wrG1aV9p', // Production LIFF ID
-    apiBaseUrl: 'https://sales-tracker-api-dot-salesappfkt.as.r.appspot.com/api', // GCP Production API URL
+    apiBaseUrl: 'https://salesappfkt.as.r.appspot.com/api', // GCP Production API URL
     teamId: null, // Will be set automatically from group chat
-    VERSION: '3.6.5' // Current version
+    VERSION: '3.6.8' // Current version
 };
 
 // Export for use in other files
