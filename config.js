b3500895f5a036d0a4eacaedcca25bd82ae9e522
kri-ruj <krittameth.rujirachainon@gmail.com>
2025-06-18 14:21:56 +0700

    chore: Bump version to 3.7.15 - Deploy Freshket branding to production

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ if (typeof window !== 'undefined' && typeof window.CONFIG === 'undefined') {
             ? 'http://localhost:10000/api' // Local development
             : 'https://sales-tracker-api-dot-salesappfkt.as.r.appspot.com/api', // GCP Production API URL - Firestore backend
         teamId: null, // Will be set automatically from group chat
-        VERSION: '3.7.14', // Current version - Backend version sync fix
+        VERSION: '3.7.15', // Current version - Freshket branding update
         googleClientId: '728428181754-2funchcf7d02p0fq8emorlfb3ttjpo3b.apps.googleusercontent.com' // Replace with your Google Client ID
     };
 }
@@ -17,6 +17,6 @@ if (typeof module !== 'undefined' && module.exports) {
         liffId: '2007552096-wrG1aV9p',
         apiBaseUrl: 'http://localhost:10000/api',
         teamId: null,
-        VERSION: '3.7.14'
+        VERSION: '3.7.15'
     };
 }
